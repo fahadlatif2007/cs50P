5b37703fa07488817c38cb661db47aa8b8164825
F <fahadlatif2007@gmail.com>
2026-05-31 14:43:31 +0000

    Sun, May 31, 2026, 5:43 PM +03:00

--- hello.py
+++ hello.py
@@ -1,4 +1,3 @@
-# This is a simple Python program that asks the user for their name and then greets them.
-x = input('what\'s your name?')
+# This is a simple Python program that asks the user for their name and then greets
+input('what\'s your name?')
 print('hello,')
-print(x)
