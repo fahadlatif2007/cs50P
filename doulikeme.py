e7374b68ea50194c31c946b7b41a272ca5c2f7eb
F <fahadlatif2007@gmail.com>
2026-06-07 13:55:40 +0000

    Sun, Jun 7, 2026, 4:55 PM +03:00

--- doulikeme.py
+++ doulikeme.py
@@ -1,12 +1,12 @@
 # do you like me?
 a = input('do you like me fadi? ').lower()
 if a in('yes', 'yea', 'yeah', 'yes my love', 'yes my baby', 'yup'):
-    print('i like you aswell')
+    print('i like you aswell ')
     input(' ')
     print('i love you sooo much meri jaan my fadi')
     input('')
-    b = input('let\'s talk')
-    c = input('hWhat\'s your fav colour fadi?')
+    b = input('let\'s talk ')
+    c = input('What\'s your fav colour fadi? ')
     if c in('blue', 'green'):
          print('SO IS MINE YAY')
     else:
