0326763417e66dfd999e99eac5e0bef9d433142d
F <fahadlatif2007@gmail.com>
2026-06-30 14:04:48 +0000

    Tue, Jun 30, 2026, 5:04 PM +03:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,12 +1,12 @@
 def main():
-    input_time = float(input("What time is it? "))
+    input_time = input("What time is it? ")
     converted_time = convert(input_time)
 
 def convert(time):
     hours, minutes = time.split(":")
     return float(hours + (minutes/60))
 
-if converted_time >= 7.0 and =< 8.0
+if converted_time >= 7.0 and <= 8.0
     print('breakfast time')
 
 if __name__ == "__main__":
