40c30c8980298419f13e7758f226bebc7ece6092
F <fahadlatif2007@gmail.com>
2026-07-01 12:27:40 +0000

    Wed, Jul 1, 2026, 3:27 PM +03:00

--- coke/coke.py
+++ coke/coke.py
@@ -3,10 +3,12 @@ while amount_due > 0:
     x = int(input("Insert coin:"))
     if x == 25 or x == 10 or x == 5:
         amount_due -= x
-    if amount_due > 0:
-        print(f"Amount due: {amount_due}")
-    elif amount_due < 0:
-        print(f"Change owed: {-amount_due}")
-        amount_due = 0
+        if amount_due > 0:
+            print(f"Amount due: {amount_due}")
+        elif amount_due < 0:
+            print(f"Change owed: {-amount_due}")
     else:
-        print("Payment complete. Thank you!")
+        print(f"Amount due: {amount_due}")
+
+
+   
