7f03ffc34f40a778ae90a08766451c3c879ed913
F <fahadlatif2007@gmail.com>
2026-07-01 08:44:58 +0000

    Wed, Jul 1, 2026, 11:44 AM +03:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,9 +1,10 @@
 #camelCase
 #ask user for camelCase
-x = input('camelCase: ')
-for i in x:
-    print(i)
-if i.isupper():
-    replace = i.replace(i, ' ' + i)
-    i = i.lower()
-    print(i)
+def main():
+    x = input('camelCase: ')
+    for i in x:
+        print(i)
+    if i.isupper():
+        replace.(i, '_' + i.lower())
+
+main()
