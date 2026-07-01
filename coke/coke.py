8b45c2627c9dc610fb21efb202fdd0b75ef603a8
F <fahadlatif2007@gmail.com>
2026-07-01 12:17:12 +0000

    Wed, Jul 1, 2026, 3:17 PM +03:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,2 +1,11 @@
 amount_due = 50
 while amount_due > 0:
+    payment = int(input("How much would you like to pay? "))
+    amount_due -= payment
+    if amount_due > 0:
+        print(f"Amount due: {amount_due}")
+    elif amount_due < 0:
+        print(f"Change owed: {-amount_due}")
+        amount_due = 0
+    else:
+        print("Payment complete. Thank you!")
