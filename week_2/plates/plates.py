37e54e676767397545e5ea7cb4ec9596160308a7
F <fahadlatif2007@gmail.com>
2026-07-04 12:24:49 +0000

    Sat, Jul 4, 2026, 3:24 PM +03:00

--- week_2/plates/plates.py
+++ week_2/plates/plates.py
@@ -9,6 +9,13 @@ def main():
 def is_valid(s):
     if len(s) < 2 or len(s) > 6:
         return False
+    if not s[0:2].isalpha():
+        return False
+    if not s.isalnum():
+        return False
+    if len(s) > 2 and s[2:].isdigit() and s[2] == '0':
+        return False
+    return True
     ...
 
 
