17003266d68d449d712dc4dbec7ac166e6fdae70
F <fahadlatif2007@gmail.com>
2026-07-11 11:56:51 +0000

    Sat, Jul 11, 2026, 2:56 PM +03:00

--- week_2/plates/plates.py
+++ week_2/plates/plates.py
@@ -1,1 +1,33 @@
-https://cs50.dev/
+def main():
+    plate = input("Plate: ").upper()
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if len(s) < 2 or len(s) > 6:
+        return False
+
+    if not s[0:2].isalpha():
+        return False
+
+    if not s.isalnum():
+        return False
+
+    seen_number = False
+
+    for i in s:
+        if i.isdigit():
+            if not seen_number:
+                if i == "0":
+                    return False
+                seen_number = True
+        elif seen_number:
+            return False
+
+    return True
+
+
+main()
