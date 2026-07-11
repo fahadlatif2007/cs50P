8f1ba4d40f10876ea661f75221829e1dc44ae38c
F <fahadlatif2007@gmail.com>
2026-07-11 11:58:59 +0000

    Sat, Jul 11, 2026, 2:58 PM +03:00

--- week_2/plates/plates.py
+++ week_2/plates/plates.py
@@ -4,20 +4,14 @@ def main():
         print("Valid")
     else:
         print("Invalid")
-
-
 def is_valid(s):
     if len(s) < 2 or len(s) > 6:
         return False
-
     if not s[0:2].isalpha():
         return False
-
     if not s.isalnum():
         return False
-
     seen_number = False
-
     for i in s:
         if i.isdigit():
             if not seen_number:
@@ -26,8 +20,5 @@ def is_valid(s):
                 seen_number = True
         elif seen_number:
             return False
-
     return True
-
-
 main()
