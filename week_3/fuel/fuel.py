b1c49a1936487058163313ffdd0f2cf7f8520ce1
F <fahadlatif2007@gmail.com>
2026-07-18 14:29:04 +0000

    Sat, Jul 18, 2026, 5:29 PM +03:00

--- week_3/fuel/fuel.py
+++ week_3/fuel/fuel.py
@@ -1,7 +1,8 @@
+def main():
+    x, y = get_fraction('Fraction: ').strip().split('/')
+    print(f'{int(z)}%')
 
-
-
-x, y = input('Fraction: ').strip().split('/')
+x, y = get_fraction('Fraction: ').strip().split('/')
 x = float(x)
 y = float(y)
 z = (x / y)*100
