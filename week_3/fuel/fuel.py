3e6b200d6776730264940bc8d4da94e0151e9d0d
F <fahadlatif2007@gmail.com>
2026-07-18 14:32:20 +0000

    Sat, Jul 18, 2026, 5:32 PM +03:00

--- week_3/fuel/fuel.py
+++ week_3/fuel/fuel.py
@@ -1,13 +1,19 @@
 def main():
     x, y = get_fraction('Fraction: ').strip().split('/')
+    x = float(x)
+    y = float(y)
+    z = (x / y)*100
     print(f'{int(z)}%')
+    if z>= 99:
+        print('Full')
+    if z<= 1:
+        print('Empty')
 
-x, y = get_fraction('Fraction: ').strip().split('/')
-x = float(x)
-y = float(y)
-z = (x / y)*100
-print(f'{int(z)}%')
-if z>= 99:
-    print('Full')
-if z<= 1:
-    print('Empty')
+def get_fraction(prompt):
+    while True:
+        try:
+            return input(prompt)
+        except (ValueError, ZeroDivisionError):
+            pass
+
+main()
