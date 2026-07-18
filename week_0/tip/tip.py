a2782c2d214ab2eef84d5207cc4285a37fe767af
F <fahadlatif2007@gmail.com>
2026-07-18 11:31:33 +0000

    Sat, Jul 18, 2026, 2:31 PM +03:00

--- week_0/tip/tip.py
+++ week_0/tip/tip.py
@@ -1,4 +1,4 @@
-def main():
+rdef main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
     tip = dollars * percent
