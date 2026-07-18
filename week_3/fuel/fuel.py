3f68606b9be4504741a93dad6ba85b9900a9b2ab
F <fahadlatif2007@gmail.com>
2026-07-18 14:23:56 +0000

    Sat, Jul 18, 2026, 5:23 PM +03:00

--- week_3/fuel/fuel.py
+++ week_3/fuel/fuel.py
@@ -5,5 +5,8 @@ x, y = input('Fraction: ').strip().split('/')
 x = float(x)
 y = float(y)
 z = (x / y)*100
-print(f'{z}%')
-
+print(f'{int(z)}%')
+if z>= 99:
+    print('Full')
+if z<= 1:
+    print('Empty')
